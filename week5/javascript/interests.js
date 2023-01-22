f957ee7a1b29ef0910bf6adc1065266a5bfb4711
mgalva-apc <119831185+mgalva-apc@users.noreply.github.com>
2023-01-22 18:46:07 +0800

    LESSON 10.1: LOOP FOR

--- week5/javascript/interests.js
+++ week5/javascript/interests.js
@@ -8,7 +8,7 @@ var txtSlides = document.getElementById("berry-slidebar").getElementsByClassName
 var index = 0;
 function showSlides(n){
     index += n;
-    for (let i=0; i<vidSlides.length; ++i) { //LESSON 10.6 iterables
+    for (let i=0; i<vidSlides.length; ++i) { //LESSON 10.6 iterables + LESSON 10.1 LOOP FOR
         vidSlides[i].style.opacity = "0";
         txtSlides[i].style.opacity = "0";
     }
