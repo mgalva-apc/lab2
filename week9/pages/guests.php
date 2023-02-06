2e5b98c3c06fb1cbadcf63ade5c5cb779c133294
mgalva-apc <119831185+mgalva-apc@users.noreply.github.com>
2023-02-06 13:11:53 +0800

    regtime

--- week9/pages/guests.php
+++ week9/pages/guests.php
@@ -21,7 +21,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. "- Email: ". $row["email"]. "- Website: ". $row["website"]. "- Comment: ". $row["comment"]. "- Gender: ". $row["gender"];
+        echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. "- Email: ". $row["email"]. "- Website: ". $row["website"]. "- Comment: ". $row["comment"]. "- Gender: ". $row["gender"]. "- reg time: ". $row["regTime";
     }
 } else {
     echo "0 results";
