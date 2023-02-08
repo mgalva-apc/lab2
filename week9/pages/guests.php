09ff848af34c57a622076a54d9187b409bdb4e9e
Bea-Alva <116138375+Bea-Alva@users.noreply.github.com>
2023-02-08 20:52:48 +0800

    revised

--- week9/pages/guests.php
+++ week9/pages/guests.php
@@ -15,13 +15,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, comment, gender, regTime FROM Outlanders";
+$sql = "SELECT id, name, email, website, comment, gender FROM Outlanders";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. "- Email: ". $row["email"]. "- Website: ". $row["website"]. "- Comment: ". $row["comment"]. "- Gender: ". $row["gender"]. "- reg time: ". $row["regTime"];
+        echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. "- Email: ". $row["email"]. "- Website: ". $row["website"]. "- Comment: ". $row["comment"]. "- Gender: ". $row["gender"];
     }
 } else {
     echo "0 results";
